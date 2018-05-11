7ea30775d806d73282c5972601ea6bafc0d96a8d
Kristjan <kristjaneimre@gmail.com>
2018-05-11 15:16:14 +0200

    subm. script updates

--- submission_scripts/run_evaluate_morbs_in_box_mpi.sh
+++ submission_scripts/run_evaluate_morbs_in_box_mpi.sh
@@ -5,18 +5,6 @@ FOLDER=..
 # Oldest .xyz file in the folder will be set as the geometry
 GEOM=$(ls -tr $FOLDER | grep .xyz | head -n 1)
 
-python3 ./evaluate_morbs_in_box_mpi.py \
-  --cp2k_input "$FOLDER"/cp2k.inp \
-  --basis_file "$FOLDER"/BR \
-  --xyz_file "$FOLDER"/$GEOM \
-  --wfn_file "$FOLDER"/PROJ-RESTART.wfn \
-  --output_file "$FOLDER"/morbs_dx0.2 \
-  --emin -2.0 \
-  --emax  2.0 \
-  --eval_region G G G G 0.0 3.0_P \
-  --dx 0.2 \
-  --eval_cutoff 14.0 \
-
 #python3 ./evaluate_morbs_in_box_mpi.py \
 #  --cp2k_input "$FOLDER"/cp2k.inp \
 #  --basis_file "$FOLDER"/BR \
@@ -25,6 +13,18 @@ python3 ./evaluate_morbs_in_box_mpi.py \
 #  --output_file "$FOLDER"/morbs_dx0.2 \
 #  --emin -2.0 \
 #  --emax  2.0 \
-#  --eval_region G G G G -2.0 3.0 \
+#  --eval_region G G G G 0.0 3.0_P \
 #  --dx 0.2 \
 #  --eval_cutoff 14.0 \
+
+python3 ./evaluate_morbs_in_box_mpi.py \
+  --cp2k_input "$FOLDER"/cp2k.inp \
+  --basis_file "$FOLDER"/BR \
+  --xyz_file "$FOLDER"/$GEOM \
+  --wfn_file "$FOLDER"/PROJ-RESTART.wfn \
+  --output_file "$FOLDER"/morbs_dx0.2 \
+  --emin -2.0 \
+  --emax  3.0 \
+  --eval_region G G G G -2.0 3.0 \
+  --dx 0.2 \
+  --eval_cutoff 14.0 \
--- submission_scripts/run_stm_sts_from_npz.sh
+++ submission_scripts/run_stm_sts_from_npz.sh
@@ -1,16 +1,28 @@
 #!/bin/bash -l
 
-FOLDER=/home/kristjan/local_work/precursor_9agnr
+FOLDER=..
+
+mkdir $FOLDER/stm_output 
 
 python3 ./stm_sts_extrap_from_npz.py \
-  --npz_file "$FOLDER"/morb_grid.npz \
-  --hartree_file "$FOLDER"/V_HARTREE-v_hartree-1_0.cube \
-  --extrap_plane 2.0 \
+  --npz_file "$FOLDER"/morbs_dx0.2.npz \
+  --hartree_file "$FOLDER"/PROJ-HART-v_hartree-1_0.cube \
+  --extrap_plane 3.0 \
   --extrap_extent 8.0 \
-  --output_dir "$FOLDER"/output \
-  --bias_voltages -0.1 0.1 \
-  --stm_plane_heights 4.0 5.0 \
+  --output_dir "$FOLDER"/stm_output \
+\
+  --bias_voltages -1.0 2.0 \
+  --stm_plane_heights 3.0 5.0 \
   --stm_isovalues  1e-7 1e-6 \
+\
   --sts_plane_heights 5.0 \
-  --sts_de 0.025 \
-  --sts_fwhm 0.05
+  --sts_de 0.05 \
+  --sts_fwhm 0.10 \
+  --sts_elim -2.0 2.0 \
+\
+  --orb_plane_heights 3.0 5.0 \
+  --n_homo 10 \
+  --n_lumo 10 \
+\
+  --skip_data_output \
+#  --skip_figs
