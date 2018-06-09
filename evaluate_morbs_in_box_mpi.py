614ab3d96627d7b4d47a419aeba54947d92f33bf
Kristjan <kristjaneimre@gmail.com>
2018-06-09 17:53:14 +0200

    Eval morbs MPI division by energy instead of Num (less memory usage)

--- evaluate_morbs_in_box_mpi.py
+++ evaluate_morbs_in_box_mpi.py
@@ -16,8 +16,8 @@ import atomistic_tools.cp2k_stm_utilities as csu
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
+mpi_rank = comm.Get_rank()
+mpi_size = comm.Get_size()
 
 parser = argparse.ArgumentParser(
     description='Calculates the molecular orbitals on a specified grid.')
@@ -94,19 +94,17 @@ parser.add_argument(
 # Define all variables that must be later broadcasted
 args = None
 cell = None
-at_positions = None
-at_elems = None
+ase_atoms = None
 basis_sets = None
-morb_composition = None
 
-print("Starting rank %d/%d"%(rank, size))
+print("Starting rank %d/%d"%(mpi_rank, mpi_size))
 
 ### -----------------------------------------
 ### SETUP (rank 0)
 ### -----------------------------------------
 setup_success = False
 try:
-    if rank == 0:
+    if mpi_rank == 0:
         args = parser.parse_args()
 
         ### -----------------------------------------
@@ -126,18 +124,12 @@ try:
         basis_sets = csu.read_basis_functions(args.basis_file, elem_basis_names)
         print("Read basis sets: %.3f" % (time.time()-time1))
 
-        time1 = time.time()
-        morb_composition, morb_energies, morb_occs, homo_inds, ref_energy = \
-            csu.load_restart_wfn_file(args.wfn_file, args.emin, args.emax)
-        print("Found %d orbitals" % len(morb_energies[0]))
-        print("Read restart: %.3f" % (time.time()-time1))
-
         setup_success = True
 finally:
     setup_success = comm.bcast(setup_success, root=0)
 
 if not setup_success:
-    print(rank, "exiting")
+    print(mpi_rank, "exiting")
     exit(0)
 
 time1 = time.time()
@@ -146,60 +138,6 @@ args = comm.bcast(args, root=0)
 cell = comm.bcast(cell, root=0)
 ase_atoms = comm.bcast(ase_atoms, root=0)
 basis_sets = comm.bcast(basis_sets, root=0)
-
-
-### -----------------------------------------
-### Divide the molecular orbitals fairly between processors
-### -----------------------------------------
-
-nspin = len(morb_composition)
-
-morb_comp_scattered = []
-
-num_morbs_spins = []
-
-for ispin in range(nspin):
-
-    if rank == 0:
-
-        num_morbs = len(morb_composition[ispin][0][0][0][0])
-        num_morbs_spins.append(num_morbs)
-        morbs_per_rank = num_morbs//size
-        extra_morbs = num_morbs % size
-
-        for i_rank in range(size):
-            ind_start = i_rank*morbs_per_rank
-            if i_rank < extra_morbs:
-                ind_start += i_rank
-                ind_end = ind_start + morbs_per_rank + 1
-            else:
-                ind_start += extra_morbs
-                ind_end = ind_start + morbs_per_rank
-            print("S%d Rank %d works with orbitals %d:%d" %(ispin, i_rank, ind_start, ind_end))
-
-            morb_comp_send = copy.deepcopy(morb_composition[ispin])
-
-            for iatom in range(len(morb_comp_send)):
-                for iset in range(len(morb_comp_send[iatom])):
-                    for ishell in range(len(morb_comp_send[iatom][iset])):
-                        for iorb in range(len(morb_comp_send[iatom][iset][ishell])):
-                            morb_comp_send[iatom][iset][ishell][iorb] = \
-                                morb_comp_send[iatom][iset][ishell][iorb][ind_start:ind_end]
-            if i_rank != 0:
-                comm.send(morb_comp_send, dest=i_rank)
-            else:
-                morb_comp_scat = copy.deepcopy(morb_comp_send)
-        # Release memory
-        morb_composition[ispin] = 0
-        morb_comp_send = 0
-    else:
-        morb_comp_scat = comm.recv(source=0)
-    
-    morb_comp_scattered.append(morb_comp_scat)
-
-
-if rank == 0:
-    print("Initial broadcast time %.4f s" % (time.time() - time1))
 
 ### -----------------------------------------
 ### Define morb evaluation region
@@ -261,7 +199,7 @@ for i in range(3):
 def minmax(arr):
     return np.array([np.min(arr), np.max(arr)])
 
-if rank == 0:
+if mpi_rank == 0:
     print("Evaluation regions (Bohr):")
     print("     x:", eval_regions[0])
     print("     y:", eval_regions[1])
@@ -281,23 +219,39 @@ dv = cell/global_size_n
 
 
 ### -----------------------------------------
+### Divide the energy range between the processors
+### -----------------------------------------
+
+emin_loc = args.emin + mpi_rank*(args.emax-args.emin)/mpi_size
+emax_loc = args.emin + (mpi_rank+1)*(args.emax-args.emin)/mpi_size - 1e-14
+
+morb_composition, morb_energies, morb_occs, homo_inds, ref_energy = \
+    csu.load_restart_wfn_file(args.wfn_file, emin_loc, emax_loc)
+
+nspin = len(morb_composition)
+
+for ispin in range(nspin):
+    num_orbs = len(morb_composition[ispin][0][0][0][0])
+    assert num_orbs == len(morb_energies[ispin])
+    print("S%d Rank %d energy range %.2f:%.2f; num orbs: %d" %(ispin, mpi_rank, emin_loc, emax_loc, num_orbs))
+
+### -----------------------------------------
 ### Calculate the molecular orbitals in the specified region
 ### -----------------------------------------
 
 morb_grids = csu.calc_morbs_in_region(cell, global_size_n,
                 ase_atoms,
-                basis_sets, morb_comp_scattered,
+                basis_sets, morb_composition,
                 x_eval_region = eval_regions[0],
                 y_eval_region = eval_regions[1],
                 z_eval_region = eval_regions[2],
                 eval_cutoff = args.eval_cutoff,
-                print_info = (rank == 0))
-
-nspin = len(morb_grids)
+                print_info = (mpi_rank == 0))
 
 grid_shape = morb_grids[0][0].shape
 
 morb_grids_collected = []
+morb_energies_collected= []
 
 for ispin in range(nspin):
 
@@ -305,7 +259,15 @@ for ispin in range(nspin):
     # Collect local array sizes using the high-level mpi4py gather
     sendcounts = np.array(comm.gather(len(morb_grids_rav), 0))
 
-    if rank == 0:
+    # Collect the numbers of orbitals
+    num_morbs = np.array(comm.gather(len(morb_grids[ispin]), 0))
+
+    # Collect energies
+    morb_en_gather = comm.gather(morb_energies[ispin], root=0)
+    if mpi_rank == 0:
+        morb_energies_collected.append(np.hstack(morb_en_gather))
+
+    if mpi_rank == 0:
         print("spin {} sendcounts: {}, total: {}".format(ispin, sendcounts, sum(sendcounts)))
         recvbuf = np.empty(sum(sendcounts), dtype=float)
     else:
@@ -313,12 +275,13 @@ for ispin in range(nspin):
 
     comm.Gatherv(sendbuf=morb_grids_rav, recvbuf=(recvbuf, sendcounts), root=0)
 
-    if rank == 0:
-        morb_grids_collected.append(recvbuf.reshape(num_morbs_spins[ispin],
+    if mpi_rank == 0:
+        total_num_morbs = np.sum(num_morbs)
+        morb_grids_collected.append(recvbuf.reshape(total_num_morbs,
                 grid_shape[0], grid_shape[1], grid_shape[2]))
         
 
-if rank == 0:
+if mpi_rank == 0:
 
     coord_arrays = []
     for i in range(3):
@@ -330,7 +293,7 @@ if rank == 0:
     geom_base = os.path.basename(args.xyz_file)
     geom_label = os.path.splitext(geom_base)[0]
 
-    time0 = time.time()
+    time1 = time.time()
     elim = np.array([args.emin, args.emax])
 
     mol_bbox = np.array([np.min(ase_atoms.positions[:, 0]),
@@ -343,29 +306,30 @@ if rank == 0:
     if nspin == 1:
         np.savez(args.output_file,
             morb_grids_s1=morb_grids_collected[0],
-            morb_energies_s1=morb_energies[0],
+            morb_energies_s1=morb_energies_collected[0],
             homo_s1=homo_inds[0][0],
             x_arr=coord_arrays[0], # Bohr
             y_arr=coord_arrays[1], # Bohr
             z_arr=coord_arrays[2], # Bohr
-            mol_bbox=mol_bbox,
+            mol_bbox=mol_bbox, # Bohr
             elim=elim,
             ref_energy=ref_energy,
             geom_label=geom_label)
     else:
         np.savez(args.output_file,
             morb_grids_s1=morb_grids_collected[0],
-            morb_energies_s1=morb_energies[0],
+            morb_energies_s1=morb_energies_collected[0],
             homo_s1=homo_inds[0][0],
             morb_grids_s2=morb_grids_collected[1],
-            morb_energies_s2=morb_energies[1],
+            morb_energies_s2=morb_energies_collected[1],
             homo_s2=homo_inds[0][1],
             x_arr=coord_arrays[0], # Bohr
             y_arr=coord_arrays[1], # Bohr
             z_arr=coord_arrays[2], # Bohr
-            mol_bbox=mol_bbox,
+            mol_bbox=mol_bbox, # Bohr
             elim=elim,
             ref_energy=ref_energy,
             geom_label=geom_label)
 
-    print("Saved the orbitals to file: %.2fs" % (time.time() - time0))
+    print("Saved the orbitals to file: %.2fs" % (time.time() - time1))
+    print("Total time taken for the whole run: %.2fs" % (time.time() - time0))
--- submission_scripts/run_evaluate_morbs_in_box_mpi_DAINT.sh
+++ submission_scripts/run_evaluate_morbs_in_box_mpi_DAINT.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #SBATCH --job-name="morb_calc"
 #SBATCH --nodes=1 # the number of ranks (total)
-#SBATCH --ntasks-per-node=12 # the number of ranks per node
+#SBATCH --ntasks-per-node=4 # the number of ranks per node
 #SBATCH --cpus-per-task=1 # use this for threaded applications
-#SBATCH --time=3:00:00 
+#SBATCH --time=2:00:00 
 #SBATCH --constraint=gpu
 #SBATCH --partition=normal
 
@@ -11,7 +11,8 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export CRAY_CUDA_MPS=1
 module load daint-gpu
 
-module load cray-python/17.09.1
+module load cray-python/3.6.1.1
+export PYTHONPATH=$PYTHONPATH:"/users/keimre/soft/ase"
 
 ulimit -s unlimited
 
@@ -20,20 +21,20 @@ FOLDER=..
 # Oldest .xyz file in the folder will be set as the geometry
 GEOM=$(ls -tr $FOLDER | grep .xyz | head -n 1)
 
-#srun -n $SLURM_NTASKS python3 ./evaluate_morbs_in_box_mpi.py \
+#srun -n $SLURM_NTASKS --mpi=pmi2 python3 ./evaluate_morbs_in_box_mpi.py \
 #  --cp2k_input "$FOLDER"/cp2k.inp \
 #  --basis_file "$FOLDER"/BR \
 #  --xyz_file "$FOLDER"/$GEOM \
 #  --wfn_file "$FOLDER"/PROJ-RESTART.wfn \
 #  --output_file "$FOLDER"/morbs_dx0.2 \
 #  --emin -2.0 \
-#  --emax  2.0 \
+#  --emax  3.0 \
 #  --eval_region G G G G 4.0t 4.0t \
 #  --dx 0.2 \
 #  --eval_cutoff 14.0 \
 #  | tee eval_morbs_in_box_mpi.out
 
-srun -n $SLURM_NTASKS python3 ./evaluate_morbs_in_box_mpi.py \
+srun -n $SLURM_NTASKS --mpi=pmi2 python3 ./evaluate_morbs_in_box_mpi.py \
   --cp2k_input "$FOLDER"/cp2k.inp \
   --basis_file "$FOLDER"/BR \
   --xyz_file "$FOLDER"/$GEOM \
