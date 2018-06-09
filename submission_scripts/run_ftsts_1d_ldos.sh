1016f9bbc05e256db69b62723483b7e4371b409f
Kristjan <kristjaneimre@gmail.com>
2018-06-09 17:55:32 +0200

    submission script examples update

--- submission_scripts/run_ftsts_1d_ldos.sh
+++ submission_scripts/run_ftsts_1d_ldos.sh
@@ -7,17 +7,23 @@ mkdir $FOLDER/sts_output
 python3 ./ftsts_1d_ldos_from_npz.py \
   --npz_file "$FOLDER"/morbs_dx0.2.npz \
   --output_dir "$FOLDER"/sts_output \
-  --sts_plane_height 5.0 \
-  --sts_de 0.01 \
-  --sts_fwhm 0.02 0.1 \
   --hartree_file "$FOLDER"/PROJ-HART-v_hartree-1_0.cube \
+\
+  --sts_plane_height 0.5 \
+  --sts_de 0.005 \
+  --sts_fwhm 0.01 \
+  --sts_line_pos -4.0 \
+  --sts_line_fwhm 0.5 \
+\
   --crop_dist_l 0 \
   --crop_dist_r -1 \
   --crop_defect_l 0 \
   --crop_defect_r 0 \
   --padding_x 600.0 \
+\
   --emin -0.5 \
   --emax 0.5 \
+\
   --lat_param 12.935 \
   --gammas 1.0 \
-  --vmax_coefs 1.0 0.5
+  --vmax_coefs 1.0
--- submission_scripts/run_orbital_analysis.sh
+++ submission_scripts/run_orbital_analysis.sh
@@ -1,92 +1,19 @@
 #!/bin/bash -l
 
-FOLDER=.
+FOLDER=..
 
 cd $FOLDER
 
-rm -rf sts_output
-mkdir sts_output
-
-# --------------------------------------
-# Cropping in SPACE
-
-CROP_DIST_L=5.0
-CROP_DIST_R=5.0
-CROP_DEF_L=1
-CROP_DEF_R=1
-
-if ls | grep -q cnt120-L60
-then
-  CROP_DIST_L=256.95
-  CROP_DIST_R=363.23
-  CROP_DEF_L=0
-  CROP_DEF_R=0
-fi
-
-if ls | grep -q cnt120-L50
-then
-  CROP_DIST_L=192.13
-  CROP_DIST_R=328.27
-  CROP_DEF_L=0
-  CROP_DEF_R=0
-fi
-
-if ls | grep -q cnt120-L100-15
-then
-  CROP_DIST_L=443.13
-  CROP_DIST_R=579.27
-  CROP_DEF_L=0
-  CROP_DEF_R=0
-fi
-
-if ls | grep -q cnt120-L12
-then
-  CROP_DIST_L=14.26
-  CROP_DIST_R=120.54
-  CROP_DEF_L=0
-  CROP_DEF_R=0
-fi
-
-if ls | grep -q cnt120-L2-
-then
-  CROP_DIST_L=14.80
-  CROP_DIST_R=27.40
-  CROP_DEF_L=0
-  CROP_DEF_R=0
-fi
-
-if ls | grep -q cnt120-L15
-then
-  CROP_DIST_L=18.00
-  CROP_DIST_R=154.00
-  CROP_DEF_L=0
-  CROP_DEF_R=0
-fi
-
-if ls | grep -q cnt1212-L50
-then
-  CROP_DIST_L=192.13
-  CROP_DIST_R=328.27
-  CROP_DEF_L=0
-  CROP_DEF_R=0
-fi
-
-if [ $CROP_DEF_L == 1 ]
-then
-  echo "#### Warning: No custom cropping defined, orb analysis won't work ####"
-  echo "#### Folder:" $(pwd)
-fi
-
-# --------------------------------------
+mkdir orb_output
 
 python3 ./orbital_analysis_from_npz.py \
-  --npz_file "$FOLDER"/morbs_h1_dx0.2.npz \
-  --output_dir "$FOLDER"/sts_output \
+  --npz_file "$FOLDER"/morbs_dx0.2.npz \
+  --output_dir "$FOLDER"/orb_output \
   --sts_plane_height 1.0 \
-  --emin -0.7 \
-  --emax 0.7 \
+  --emin -0.5 \
+  --emax 0.5 \
   --lat_param 4.26 \
-  --crop_x_l $CROP_DIST_L \
-  --crop_x_r $CROP_DIST_R \
+  --crop_x_l 0.0 \
+  --crop_x_r -1.0 \
   --work_function 4.36
 
